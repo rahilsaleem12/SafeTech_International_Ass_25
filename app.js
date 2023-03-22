d1e08e370ee70bdad8a7305d826b77ad24025a20
Rahilsaleem12 <rahilsaleem565@gmail.com>
2023-03-22 17:17:24 +0500

    some code

--- app.js
+++
@@ -1,6 +0,0 @@
-let bar = document.querySelector("#bars")
-let navbar = document.querySelector(".header .Navbar")
-bar.onclick = ()=>{
-    bar.classList.toggle('fa-times')
-    navbar.classList.toggle('active')
-}
